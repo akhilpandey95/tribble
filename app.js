85dfd0b6006e3687cb6c22f9cf2f82ea150f5059
AkhilHector <akhilhector.1@gmail.com>
2015-06-05 16:07:58 +0530

    Added favicon,markup changes to index and request parsing

--- app.js
+++ app.js
@@ -3,12 +3,30 @@
  * 2. For curr page = http://www.engadget.com/
  * 3. Example of the url : http://www.engadget.com/2015/06/04/apple-watch-reader-review-roundup/
  */
-
+var e = require('express')
 var c = require('cheerio')
 var fs = require('fs')
 var req = require('request')
+var post = require('request')
+var store = new Array();
+var app = e();
+var router = e.Router();
+var port = process.env.PORT || 8100;
 
+app.use(e.static('assets'));
+
+console.log("Express Serving is starting")
 console.log("Starting Scraping")
+
+router.get('/', function(req, res) {
+        var data = fs.readFileSync('index.html', 'utf8');
+        res.send(data.toString());
+});
+
+app.use('/', router);
+app.listen(port, function() {
+        console.log("Listening on port " + port);
+});
 
 req('http://www.engadget.com/', function(err, res, html) {
                 if(!err && res.statusCode == 200) {
@@ -17,9 +35,12 @@ req('http://www.engadget.com/', function(err, res, html) {
                         // Gives all the headlines together
                         l('header.post-header').each(function(i, element) {
                                 var content = l(this).children().children().children('.h2');
-                                console.log(content.text());
+                                var data = content.text();
+                               // console.log(data);
+                                store.push(data);
+                                console.log(store);
                         });
-                        
+
                         // Gives the top 5 current posts on the page
                         l('div').attr('id','carousel').each(function(i, element) {
                                 var content = l(this).children().children('.always');
@@ -37,6 +58,17 @@ req('http://www.engadget.com/', function(err, res, html) {
                                 var content = l(this);
                                 console.log(content.text());
                        });
-
                 }
                 });
+
+post('http://localhost:8100/', function(err, res, html) {
+        if(!err && res.statusCode == 200) {
+                var p = c.load(html);
+                p('h3.panel-title').each(function(i, element) {
+                        var content = p(this);
+                        console.log(content.text());
+                });
+        }
+});
+
+
