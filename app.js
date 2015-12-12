93358495f28d9553e0dfb4b7eefa6934b283821e
AkhilHector <akhilhector.1@gmail.com>
2015-12-13 03:56:56 +0530

    Updated Readme and added another Request to the scraping list
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- app.js
+++ app.js
@@ -41,6 +41,19 @@ req('http://www.reddit.com/r/technology', function(err, res, html) {
                 }
                 });
 
+req('http://www.gizmodo.in/gadgets', function(err,res, html) {
+                if(!err && res.statusCode == 200) {
+                        var l = c.load(html);
+                        
+                        // Headlines from the Gadgets page of Gizmodo
+                        l('article.article').each(function (i, element) {
+                                var content = l(this).children().children().children('h2');
+                                var data = content.text();
+                                console.log(data);
+                        });
+                }
+                });
+
 req('http://www.engadget.com/', function(err, res, html) {
                 if(!err && res.statusCode == 200) {
                         var l = c.load(html);
